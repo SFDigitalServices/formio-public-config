f637eb0d38d9109a10f6414fe5640e05beaec0c0
Shawn Allen <shawnbot@users.noreply.github.com>
2021-12-13 16:37:37 -0800

    add mutation observer for .formio-form

--- preview.js
+++ preview.js
@@ -1,2 +1,16 @@
 // form preview javascript
 console.log('hello from preview.js!')
+
+const observer = new MutationObserver(mutations => {
+  const selector = '.formio-form'
+  const el = mutations
+    .map(mute => mute.target.matches(selector) ? mute.target : mute.target.querySelector(selector))
+    .find(Boolean)
+  if (el && !el.hasAttribute('data-hijacked')) {
+    el.setAttribute('data-hijacked', true)
+    const url = Object.values(Formio.forms).pop().url
+    console.log('[sfds] hijacked:', el, url)
+  }
+})
+
+observer.observe(document.body, { childList: true, subtree: true })
