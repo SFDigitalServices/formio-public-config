b48c7a74b4864af8f2a3f80ccdadb90c88a60a3a
Shawn Allen <shawnbot@users.noreply.github.com>
2021-12-13 20:43:06 -0800

    get the canonical form URL

--- preview.js
+++ preview.js
@@ -1,15 +1,7 @@
 // form preview javascript
-console.log('hello from preview.js!')
-
-;(() => {
-  const styles = [
-    'https://formio-sfds.herokuapp.com/sfgov/forms.css'
-  ]
-  const scripts = [
-    'https://unpkg.com/formiojs@4.13.1/dist/formio.full.min.js',
-    'https://unpkg.com/formio-sfds@9.2.2/dist/formio-sfds.standalone.js'
-  ]
-
+(() => {
+  console.log('[sfds] preview')
+  
   const observer = new MutationObserver(mutations => {
     const el = mutations
       .map(({ target }) => nearest(target, '.formio-form'))
@@ -18,7 +10,12 @@ console.log('hello from preview.js!')
     const attr = 'data-hijacked'
     if (el && !el.hasAttribute(attr)) {
       el.setAttribute(attr, true)
-      const url = Object.values(window.Formio.forms).pop().url
+      const form = Object.values(window.Formio.forms).pop()
+      console.log('[sfds] form:', form)
+      const [projectId, formName] = form.machineName.split(':')
+      const { host } = new URL(form.url)
+      const url = `${host}/${projectId}/${formName}`
+      console.log('[sfds] rendering preview for: %s', url)
       const preview = renderPreview(url)
       el.hidden = true
       el.parentNode.insertBefore(preview, el)
@@ -28,6 +25,14 @@ console.log('hello from preview.js!')
   observer.observe(document.body, { childList: true, subtree: true })
 
   function renderPreview (url) {
+    const styles = [
+      'https://formio-sfds.herokuapp.com/sfgov/forms.css'
+    ]
+    const scripts = [
+      'https://unpkg.com/formiojs@4.13.1/dist/formio.full.min.js',
+      'https://unpkg.com/formio-sfds@9.2.2/dist/formio-sfds.standalone.js'
+    ]
+
     const iframe = document.createElement('iframe')
     iframe.style.width = '100%'
     iframe.style.height = '800px'
@@ -39,7 +44,7 @@ console.log('hello from preview.js!')
       ${scripts.map(src => `<script src="${src}"></script>`).join('\n')}
       <script>
         Formio.createForm(document.getElementById('formio'), ${JSON.stringify(url)})
-          .then(form => alert('form created!'))
+          .then(form => alert('[sfds] form ready!'))
       </script>
     `
     return iframe
