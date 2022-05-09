2eea1632eb546db7f369d22beafd541227305979
Corey Hunt <corey.hunt@gmail.com>
2022-05-09 12:13:45 -0700

    Media query test for the table

--- preview.js
+++ preview.js
@@ -100,14 +100,13 @@
       // Narrows the form title container width
       'td .row .col-sm-8': el => {
         el.classList.remove('col-sm-8')
-        el.classList.add('col-sm-6')
-      classify(el, ['w-full'])
+      classify(el, ['w-full', 'lg:col-sm-6'])
       },
       // Widens the button div and adds some formatting
       'td .row .col-sm-4': el => {
       el.classList.remove('col-sm-4')
-        el.classList.add('col-sm-6', 'flex', 'justify-end')
-          classify(el, ['w-full'])
+        el.classList.add('flex', 'justify-end')
+          classify(el, ['w-full', 'lg:col-sm-6'])
       },
       // Changes "Edit Data" button text to "Preview"
       '.form-btn.form-btn-use': el => {
