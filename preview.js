7caab8b8e267bb0328bec35052cf071d82c846e1
Shawn Allen <shawnbot@users.noreply.github.com>
2022-05-18 11:19:54 -0700

    Update preview.js

--- preview.js
+++ preview.js
@@ -180,7 +180,7 @@
       '.pull-right.btn': el => {
         const li = document.querySelector('app-form > .nav-tabs li').cloneNode()
         if (moveElement(el, 'app-form', 'app-form > ul.nav-tabs', 'li:nth-child(4)')) {
-          el.className = [navLinkClasses, ...navLinkInactiveClasses].join(' ')
+          el.className = [navLinkClasses, navLinkInactiveClasses].flat().join(' ')
           el.parentNode.insertBefore(el, li)
           li.appendChild(el)
         }
